a60d8521c0f3a5ee985ae3bddadbdc593de0de2b
vedsa <dark.lightghost70@gmail.com>
2017-10-18 23:37:50 +0200

    test
    modifier pour test

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from spider import Spider
 from domain import *
 from general import *
 
-PROJECT_NAME = 'viper-seo'
-HOMEPAGE = 'http://viper-seo.com/'
+PROJECT_NAME = 'test'
+HOMEPAGE = 'http://test.com/'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
